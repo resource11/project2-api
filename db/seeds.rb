458305285c94a5259b3387499d58bf61db846c27
resource11 <mcmahon@resource11.com>
2015-11-11 00:50:33 -0500

    Successful creation of test seeds.rb file for testing purposes with users, bikes, and favorites table.
    Next steps, move this data into an example_data.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,6 @@ User.delete_all
 
 5.times do | i |
   puts "Creating User: user#{i}@example.com"
-  # User.create!(email: FFaker::Internet.email, password: "password", password_confirmation: "password")
   user = User.create!(email: "user#{i}@example.com", password: "a", password_confirmation: "a")
 
   rand(1..4).times do |bike_num|
@@ -33,18 +32,10 @@ User.delete_all
 end
 
 b1 = Bike.last
-puts "#{b1.id}"
-u1 = Bike.last
-favorite_bike = FavoriteBike.create!()
+u1 = User.last
+u1.id = User.last.id
+b1.id = Bike.last.id
+favorite_bike = FavoriteBike.create!(favorite: true, user_id: u1.id, bike_id: b1.id)
 u1.favorite_bikes << favorite_bike
-  puts "favorite_bike is #{favorite_bike.id}"
-
-  # favorite_bike = FavoriteBike.create!(favorite: true, user: "#{user(id)}", bike: "#{bikes(id)}")
-  # puts "favorite_bike is #{favorite_bike}"
-
-  # 3.times do |fav_bike|
-  #   puts "Creating Favorite_bike: bike_favorite_#{favorite_bike}_#{fav_bike}"
-  #   favorite_bike.create!(favorite: true)
-  # end
-
+puts "favorite_bike is #{favorite_bike.id}"
 
